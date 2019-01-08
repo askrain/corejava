efa1bca9500a27eec5aeabc2580c776d82a0b0b5
Stem <stem.root.tech@gmail.com>
2019-01-09 00:04:49 +0800

    增加注释

--- v2ch01/streams/CountLongWords.java
+++ v2ch01/streams/CountLongWords.java
@@ -13,18 +13,19 @@ public class CountLongWords
    {
       String contents = new String(Files.readAllBytes(
             Paths.get("../gutenberg/alice30.txt")), StandardCharsets.UTF_8);
-      List<String> words = Arrays.asList(contents.split("\\PL+"));
-
+      List<String> words = Arrays.asList(contents.split("\\PL+"));//以非字母间隔生成字符串数组
+      System.out.println("words's length="+words.size());//29075
       long count = 0;
       for (String w : words)
       {
          if (w.length() > 12) count++;
       }
-      System.out.println(count);
+      System.out.println(count);//以下的打印都是27
 
+      //集合转换为顺序流  filter起过滤作用,产生一个流，其中包含所有满足条件的所有元素  count()产生当前流中的元素数量，是个终结操作
       count = words.stream().filter(w -> w.length() > 12).count();
       System.out.println(count);
-
+      //集合转换为并行流
       count = words.parallelStream().filter(w -> w.length() > 12).count();
       System.out.println(count);
    }
